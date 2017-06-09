69fa3c4128a24ec0323455715cd5832b4d37a479
Marvin Rösch <marvinroesch99@gmail.com>
2017-06-09 02:26:41 +0200

    Update markup and script to reflect sidebar changes

--- forge_theme/js/theme.js
+++ forge_theme/js/theme.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
+    // Sidebars always are in asides
     var elementPosition = $('aside').offset();
     $(window).scroll(function () {
         var mq = window.matchMedia("(max-width: 1080px)");
         if (!mq.matches && $(window).scrollTop() >= elementPosition.top - 16) {
-            $('aside').css('position', 'fixed').css('top', '1rem');
+            $('.sidebar-sticky aside').css('position', 'fixed').css('top', '1rem');
         } else {
-            $('aside').css('position', 'relative').css('top', '0');
+            $('.sidebar-sticky aside').css('position', 'relative').css('top', '0');
         }
     });
     $('.scroll-pane').jScrollPane({
@@ -14,30 +15,36 @@ $(document).ready(function () {
         hideFocus: true
     });
     $('.open-sidebar').click(function (e) {
-        $('.aside-wrapper').addClass('active-sidebar');
+        $('.sidebar-sticky').addClass('active-sidebar');
         $('body').addClass('sidebar-active');
         e.preventDefault();
     });
     $('.close-sidebar').click(function (e) {
-        $('.aside-wrapper').removeClass('active-sidebar');
+        $('.sidebar-sticky').removeClass('active-sidebar');
         $('body').removeClass('sidebar-active');
         e.preventDefault();
     });
+    // Collapsible elements implementation
     $('.nav-collapsible:not(.nav-collapsible-open)').css('display', 'none');
     var icons = $('.nav-collapsible-open').siblings('.toggle-collapsible').children('.collapsible-icon');
+    var texts = $('.nav-collapsible-open').siblings('.toggle-collapsible').children('.collapsible-text');
     icons.removeClass('fa-plus');
     icons.addClass('fa-minus');
+    texts.html('Show');
     $('.toggle-collapsible').click(function (e) {
         var collapsible = $(this).siblings('.nav-collapsible');
         var icon = $(this).children('.collapsible-icon');
+        var text= $(this).children('.collapsible-text');
         if (collapsible.css('display') == 'none') {
             collapsible.css('display', 'block');
             icon.addClass('fa-minus');
-            icon.removeClass('fa-plus')
+            icon.removeClass('fa-plus');
+            text.html('Hide');
         } else {
             collapsible.css('display', 'none');
             icon.addClass('fa-plus');
-            icon.removeClass('fa-minus')
+            icon.removeClass('fa-minus');
+            text.html('Show');
         }
         e.preventDefault();
     });
