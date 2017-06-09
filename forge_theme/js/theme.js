7b44e7f5361c9aba8eff8e595536d88706a1ab74
Marvin Rösch <marvinroesch99@gmail.com>
2017-06-09 15:31:56 +0200

    Make highlighting more robust by doing a first step in the Markdown compilation process Use a CDN for foreign scripts rather than shipping them directly Fix tables exceeding page width on small screens

--- forge_theme/js/theme.js
+++ forge_theme/js/theme.js
@@ -48,5 +48,12 @@ $(document).ready(function () {
         }
         e.preventDefault();
     });
-    hljs.initHighlightingOnLoad();
+    $("pre.highlight code[class*='language-']").each(function() {
+        var className = this.className.match(/language-([A-Za-z0-9+-]+)/);
+        if (className) {
+            $(this).removeClass(className[0]);
+            $(this).addClass(className[1].toLowerCase());
+        }
+    });
+    hljs.initHighlighting();
 });
